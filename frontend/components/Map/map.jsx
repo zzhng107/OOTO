1e2c36aa1ecd3fd1c3cfdf322fbbe1d8ca513742
Zhiwei Zhang <zzw408673530@gmail.com>
2017-11-12 01:21:18 -0600

    fronend

---
+++ frontend/components/Map/map.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+class googleMap extends React.Component {
+   render() {
+      return (
+         <div>
+            Hello World ReactJSssss!!!
+         </div>
+      );
+   }
+}
+export default googleMap;
---
+++ frontend/components/Planner/planner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+class Planner extends React.Component {
+   render() {
+      return (
+         <div>
+            Hello World ReactJSssss!!!
+         </div>
+      );
+   }
+}
+export default Planner;
