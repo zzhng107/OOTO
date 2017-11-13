f13cf9326ca7370ca0afe160b7b8d76cb7c79e2a
Zhiwei Zhang <zzw408673530@gmail.com>
2017-11-13 16:02:45 -0600

    front

--- frontend/components/Planner/planner.jsx
+++ frontend/components/Planner/planner.jsx
@@ -3,26 +3,87 @@ import style from './planner.scss';
 var axios = require('axios');
 
 class Planner extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      days: 1,
+      days_plan: [],
+    };
+  }
+
+  renderDays() {
+    let days_plan_ = [];
+    for (let i = 1; i <= this.state.days; i++) {
+      days_plan_.push(<DaysComponents key={i} number={i} deletefunc={(index) => this.remove_day(index)} />);
+    }
+    this.setState({
+      days_plan: days_plan_,
+    })
+  }
+
+  addDays() {
+    this.setState({
+      days: this.state.days + 1,
+    }, () => {
+      this.renderDays();
+    })
+  }
+
+  remove_day(index) {
+    // let temp = this.state.days_plan.slice();
+    // console.log(index);
+    // console.log(temp.filter(function(val,ind){return ind!=index}));
+    // temp = temp.filter(function(val,ind){return ind!=index});
+    // console.log(temp);
+    
+    // console.log("inside");  
+    this.setState({
+      days: this.state.days - 1,
+      // days_plan: temp,
+    }, () => {
+      this.renderDays();
+    })
+  }
+
+  componentDidMount() {
+    this.renderDays();
+  }
+
   render() {
     return (
       <div class="ui raised segments">
-        <div class="ui segment">Day 1
-          <div class="ui input">
-            <input type="text" placeholder="Search..." />
-          </div>
-        </div>
-        <div class="ui segment">Day 2
-          <div class="ui input">
-            <input type="text" placeholder="Search..." />
-          </div>
-        </div>
-        <div class="ui segment">Day 3
-          <div class="ui input">
-            <input type="text" placeholder="Search..." />
+        {this.state.days_plan}
+        <div class="ui segment">
+          <div id="control_button">
+            <button class="ui grey button" role="button" onClick={() => this.addDays()}> + </button>
+            <button class="ui primary button" role="button"> Save </button>
           </div>
         </div>
       </div>
     );
   }
 }
+
+class DaysComponents extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onClickHandler() {
+    this.props.deletefunc(this.props.number);
+  }
+
+  render() {
+    return (
+      <div class="ui segment">Day {this.props.number}
+        <div class="ui input">
+          <input type="text" />
+        </div>
+        <button class="ui mini circular button" role="button" onClick={() => this.onClickHandler()}> - </button>
+      </div>
+    );
+  }
+}
+
+
 export default Planner;
