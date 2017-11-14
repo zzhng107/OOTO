d042bea77e7a0ddbe87fc3bd816d0e595928c79c
Zhiwei Zhang <zzw408673530@gmail.com>
2017-11-13 21:23:02 -0600

    front

--- frontend/components/Planner/planner.jsx
+++ frontend/components/Planner/planner.jsx
@@ -7,52 +7,60 @@ class Planner extends React.Component {
     super(props);
     this.state = {
       days: 1,
-      days_plan: [],
+      days_largest: 0,
+      days_plan: [<DaysComponents key={0} number={0} deletefunc={(index) => this.remove_day(index)} />],
     };
   }
 
-  renderDays() {
-    let days_plan_ = [];
-    for (let i = 1; i <= this.state.days; i++) {
-      days_plan_.push(<DaysComponents key={i} number={i} deletefunc={(index) => this.remove_day(index)} />);
-    }
-    this.setState({
-      days_plan: days_plan_,
-    })
-  }
+  // renderDays() {
+  //   let days_plan_ = [];
+  //   for (let i = 1; i <= this.state.days; i++) {
+  //     days_plan_.push(<DaysComponents key={days_largest+1} number={days_largest+1} deletefunc={(index) => this.remove_day(index)} />);
+  //   }
+  //   this.setState({
+  //     days_largest: this.state.days_largest+1,
+  //     days_plan: days_plan_,
+  //   })
+  // }
 
   addDays() {
+    let temp = this.state.days_plan.slice();
+    temp.push(
+
+      <DaysComponents key={this.state.days_largest + 1} number={this.state.days_largest + 1} deletefunc={(index) => this.remove_day(index)} />
+    );
     this.setState({
-      days: this.state.days + 1,
+      days_largest: this.state.days_largest + 1,
+      days_plan: temp,
     }, () => {
-      this.renderDays();
+
     })
   }
 
   remove_day(index) {
-    // let temp = this.state.days_plan.slice();
-    // console.log(index);
-    // console.log(temp.filter(function(val,ind){return ind!=index}));
-    // temp = temp.filter(function(val,ind){return ind!=index});
-    // console.log(temp);
-    
-    // console.log("inside");  
+    let temp = this.state.days_plan.slice();
+    temp = temp.filter((val) => { console.log(val); return val.props.number != index });
     this.setState({
-      days: this.state.days - 1,
-      // days_plan: temp,
+      days_plan: temp,
     }, () => {
-      this.renderDays();
+      // this.renderDays();
     })
   }
 
   componentDidMount() {
-    this.renderDays();
+    // this.renderDays();
   }
 
   render() {
     return (
       <div class="ui raised segments">
-        {this.state.days_plan}
+        {this.state.days_plan.map((val, ind) => {
+          return (
+            <div class="ui segment">Day {ind}
+              {val}
+            </div>
+          )
+        })}
         <div class="ui segment">
           <div id="control_button">
             <button class="ui grey button" role="button" onClick={() => this.addDays()}> + </button>
@@ -70,20 +78,18 @@ class DaysComponents extends React.Component {
   }
 
   onClickHandler() {
+    console.log(Math.random() * 1000);
     this.props.deletefunc(this.props.number);
   }
 
   render() {
     return (
-      <div class="ui segment">Day {this.props.number}
-        <div class="ui input">
-          <input type="text" />
-        </div>
+      <div class="ui input">
+        <input type="text" />
         <button class="ui mini circular button" role="button" onClick={() => this.onClickHandler()}> - </button>
       </div>
     );
   }
 }
-
 
 export default Planner;
