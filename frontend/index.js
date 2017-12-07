168ae827488d8d2f2558326c4ab5895443317bc2
Zhiwei Zhang <zzhng107@dcl-l440-02.ews.illinois.edu>
2017-12-07 15:56:03 -0600

    asd

--- frontend/index.js
+++ frontend/index.js
@@ -61,7 +61,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "1e9ead8f92aec062f9ed"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "12658a416c8e2719488b"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -1591,7 +1591,7 @@ exports = module.exports = __webpack_require__(10)(undefined);
 
 
 // module
-exports.push([module.i, ".ui.raised.segments {\n  position: absolute;\n  width: 100%; }\n\n.ui.input {\n  margin-left: 5%; }\n\n.ui.circular.button {\n  position: relative;\n  left: 5%; }\n\n#control_button button {\n  position: relative;\n  width: 100%; }\n", ""]);
+exports.push([module.i, ".ui.raised.segments {\n  position: absolute;\n  width: 100%; }\n\n.ui.input {\n  margin-left: 5%; }\n\n.ui.circular.button {\n  position: relative;\n  left: 5%; }\n\n#control_button button {\n  position: relative;\n  width: 100%; }\n\n#useForFillTripPlan {\n  visibility: hidden; }\n", ""]);
 
 // exports
 
